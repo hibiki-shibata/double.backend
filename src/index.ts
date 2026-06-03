84493683c34077fd15b85b24266d0b1392224ba3
Hibiki Shibata <hibiki@192.168.1.51>
2026-06-03 17:28:43 -0400

    Add: graceful shutdow

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@
 import { server, port } from './server.js'
 import { logger } from './shared/logger/logger.js'
 
-server.listen(port, () => {
-    logger.info(`Double Backend Server is listeing on ${port}`)
+const activeServer = server.listen(port, () => {
+    logger.info({ port }, 'Double backend started')
 })
+
+async function shutdown(signal: string) {
+    logger.warn({ signal }, 'shutdown received')
+    
+    activeServer.close(async () => {
+        // await db.end();
+        await new Promise<void>((resolved) => logger.flush(() => resolved()))
+        process.exit(0)
+    })
+
+    setTimeout(() => {
+        logger.error('Forced shutdown - Graceful shutdown failed')
+        process.exit(1)
+    }, 10000).unref()
+}
+
+process.on('SIGTERM', () => shutdown('SIGTERM'))
+process.on('SIGINT', () => shutdown('SIGINT'))
