3e030af04a6e51896ed3d3ec2dd1205e1568a781
Hibiki Shibata <hibiki@192.168.1.51>
2026-06-06 19:20:06 -0400

    Edit: tsconfig.json into *.build.json and *.test.json. pnpm test script so to read ts in test

--- app/test/shared/middleware/globalExceptionHandler.test.ts
+++ app/test/shared/middleware/globalExceptionHandler.test.ts
@@ -15,3 +15,15 @@
 
 //     return { req, res, next, json, status }
 // }
+
+// Doc: https://nodejs.org/learn/test-runner/using-test-runner
+import assert from "node:assert/strict"
+import { test } from 'node:test'
+
+
+test('GlobalExceptionHandler.test.js Test', { concurrency: true }, t => {
+
+    t.test('Fairst line of test', () => assert.equal('a', 'a'))
+    // t.test('Fairst line of test', assert.AssertionError new
+
+});
