e4118136727243a52b3deb01a02379fe02c539ca
Hibiki Shibata <hibiki@192.168.1.111>
2026-06-16 01:11:30 -0400

    Add: cachedUserRepository to user's features

--- app/src/features/user/account/index.ts
+++ app/src/features/user/account/index.ts
@@ -2,15 +2,22 @@ import type { Router } from "express"
 import type { UserRepository } from "../shared/repository/user.repository.js"
 import type { UserAccountService } from "./service/userAccount.service.js"
 import type { UserAccountController } from "./controller/userAccount.controller.js"
+import type { CacheService } from "../../../shared/infra/cache/service/cache.service.js"
 import { logger } from "../../../shared/logger/logger.js"
+import { redisClient } from "../../../shared/infra/cache/client/redisClient.js"
+import { RedisCacheService } from "../../../shared/infra/cache/service/redis.service.js"
+import { cacheKeys } from "../../../shared/config/cache.config.js"
 import { prismaClient } from "../../../shared/infra/db/prismaClient.js"
 import { PrismaUserRepository } from "../shared/repository/prisma.user.repository.js"
+import { CachedUserRepository } from "../shared/repository/cached.user.repository.js"
 import { UserAccountServiceV1 } from "./service/userAccount.service.v1.js"
 import { UserAccountControllerV1 } from "./controller/userAccount.controller.v1.js"
 import { userAccountRouter } from "./router/userAccount.router.js"
 
 export function userAccountFeature(): Router {
-    const repository: UserRepository = new PrismaUserRepository(prismaClient)
+    const cacheService: CacheService = new RedisCacheService(redisClient, logger)
+    const dbRepository: UserRepository = new PrismaUserRepository(prismaClient)
+    const repository: UserRepository = new CachedUserRepository(dbRepository, cacheService, cacheKeys)
     const service: UserAccountService = new UserAccountServiceV1(repository, logger)
     const controller: UserAccountController = new UserAccountControllerV1(service, logger)
     return userAccountRouter(controller)
--- app/src/features/user/auth/index.ts
+++ app/src/features/user/auth/index.ts
@@ -3,19 +3,26 @@ import type { UserRepository } from "../shared/repository/user.repository.js"
 import type { PasswordService } from "./service/password.service.js"
 import type { UserAuthService } from "./service/userAuth.service.js"
 import type { UserAuthController } from "./controller/userAuth.controller.js"
+import type { CacheService } from "../../../shared/infra/cache/service/cache.service.js"
 import { logger } from "../../../shared/logger/logger.js"
+import { PasswordServiceV1 } from "./service/password.service.v1.js"
 import { prismaClient } from "../../../shared/infra/db/prismaClient.js"
 import { PrismaUserRepository } from "../shared/repository/prisma.user.repository.js"
-import { cookieOptions, passwordEncoderOptions } from "../../../shared/config/security.config.js"
+import { redisClient } from "../../../shared/infra/cache/client/redisClient.js"
+import { RedisCacheService } from "../../../shared/infra/cache/service/redis.service.js"
 import { jwtTokenService } from "../../../shared/auth/index.js"
-import { PasswordServiceV1 } from "./service/password.service.v1.js"
 import { UserAuthServiceV1 } from "./service/userAuth.service.v1.js"
-import { UserAuthControllerV1 } from "./controller/userAuth.controller.v1.js"
 import { userAuthRouter } from "./router/userAuth.router.js"
+import { cacheKeys } from "../../../shared/config/cache.config.js"
+import { CachedUserRepository } from "../shared/repository/cached.user.repository.js"
+import { cookieOptions, passwordEncoderOptions } from "../../../shared/config/security.config.js"
+import { UserAuthControllerV1 } from "./controller/userAuth.controller.v1.js"
 
 export function userAuthFeature(): Router {
-    const repository: UserRepository = new PrismaUserRepository(prismaClient)
     const passwordService: PasswordService = new PasswordServiceV1(passwordEncoderOptions)
+    const cacheService: CacheService = new RedisCacheService(redisClient, logger)
+    const dbRepository: UserRepository = new PrismaUserRepository(prismaClient)
+    const repository: UserRepository = new CachedUserRepository(dbRepository, cacheService, cacheKeys)
     const service: UserAuthService = new UserAuthServiceV1(repository, passwordService, jwtTokenService, logger)
     const controller: UserAuthController = new UserAuthControllerV1(service, cookieOptions, logger)
     return userAuthRouter(controller)
